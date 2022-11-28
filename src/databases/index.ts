dc42e113f4e7f001de08dd8e2640ea87c90d833d
anandarpit <orionarpit@gmail.com>
2022-11-28 14:35:03 +0530

    reduced code for working

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -1,13 +1,12 @@
 import Sequelize from 'sequelize';
 import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from '@config';
-import UserModel from '@models/users.model';
 import { logger } from '@utils/logger';
 
 const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   dialect: 'mysql',
   host: DB_HOST,
-  port: DB_PORT,
-  timezone: '+09:00',
+  port: +DB_PORT,
+  timezone: '+05:30',
   define: {
     charset: 'utf8mb4',
     collate: 'utf8mb4_general_ci',
@@ -28,7 +27,6 @@ const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
 sequelize.authenticate();
 
 const DB = {
-  Users: UserModel(sequelize),
   sequelize, // connection instance (RAW queries)
   Sequelize, // library
 };
